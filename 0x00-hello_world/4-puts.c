6d1f8867ceaaca6101fb8f58d1b87f621606a27c
SitholeWEndy <sitholewendy12@gmail.com>
2023-02-16 07:50:03 -0800

    puts. c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.n>
+
 /**
  * main - A program that prints a line with function
  * Return: 0 (Success)
- */
+*/
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
 }
