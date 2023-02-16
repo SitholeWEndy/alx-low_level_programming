5ac360bc060b98df1a806d0e6b88bd07cef3a4a7
SitholeWEndy <sitholewendy12@gmail.com>
2023-02-16 09:11:27 -0800

    6-size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-/**
- * main - A program that print a line using the printf function
+
+i/**
+ *main - A program line function," followed by a new line.
  * Return: 0 (Success)
  */
 
