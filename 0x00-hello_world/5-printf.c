f9ad150fe547831c2d49913167409c4e8cd2b725
SitholeWEndy <sitholewendy12@gmail.com>
2023-02-16 12:39:32 -0800

    size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
-
-/**
- * main - prints "with proper grammar, but the ouutcome is
- * a piece of art," followed newline
- * Return: 0 (Success)
- *
- */
-
-int main(void)
-{
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-		Return(0);
-}
+`
