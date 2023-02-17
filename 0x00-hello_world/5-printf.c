cb7ed280dd9eee7c27b15e3341f3fa7bc5c5ceb0
SitholeWEndy <sitholewendy12@gmail.com>
2023-02-16 19:44:50 -0800

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
-#include <unistd.h>
-
+#include <stdio.h>
 /**
- * main- Prints "and 
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
